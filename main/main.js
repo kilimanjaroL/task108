f8e616752673065c57e3d4e03ff5ad61107d2f33
kilimanjaroL <2636246488@qq.com>
2017-11-19 22:14:59 +0800

    task 108

--- main/main.js
+++ main/main.js
@@ -5,13 +5,14 @@ window.onload=function (){
 };
 
 function main(){
-    var sum = count1() + count2() + count3() + count4() + count5();
-    var result = document.getElementById ("name").value + "本次测验的成绩是："+sum.toString ();
+    let sum = completion() + radio() + checkbox() + judge() + shortanswer();
+    let result = document.getElementById ("name").value + "本次测验的成绩是："+sum.toString ();
     alert (result);
+    document.getElementById ("score").value = sum;
 }
 
-function count1() {
-    var sum = 0;
+function completion() {
+    let sum = 0;
     if(document.getElementById("1.1").value === "统一建模语言" )
         sum += 5;
     if(document.getElementById("1.21").value === "封装性" )
@@ -24,39 +25,41 @@ function count1() {
     return sum;
 }
 
-function count2(){
-    var sum = 0;
-    if(document.getElementsByName ("1")[1].checked === true)
+function radio(){
+    let sum = 0;
+    if(document.getElementsByName ("2.1")[1].checked === true)
         sum += 10;
-    if(document.getElementsByName ("2")[0].checked === true)
+    if(document.getElementsByName ("2.2")[0].checked === true)
         sum += 10;
+
     return sum;
 }
 
-function count3() {
-    var sum = 0;
-    var list1 = document.getElementsByName ("first");
+function checkbox() {
+    let sum = 0;
+    let list1 = document.getElementsByName ("3.1");
     if (list1[0].checked === true && list1[1].checked === true && list1[2].checked === false && list1[3].checked === true )
         sum += 10;
-    var list2 = document.getElementsByName ("second");
+    let list2 = document.getElementsByName ("3.2");
     if (list2[0].checked === true && list2[1].checked === true && list2[2].checked === true && list2[3].checked === false )
         sum += 10;
 
     return sum;
 }
 
-function count4() {
-    var sum = 0;
+function judge() {
+    let sum = 0;
     if(document.getElementsByName ("4.1")[1].checked === true)
         sum += 10;
     if(document.getElementsByName ("4.2")[0].checked === true)
         sum += 10;
+
     return sum;
 }
 
-function count5() {
-    var sum = 0;
-    if(document.getElementById ("short-answer").value === "模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体；也可以是某种图形；或者是一种数学表达式。")
+function shortanswer() {
+    let sum = 0;
+    if(document.getElementById ("5.1").value === "模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体；也可以是某种图形；或者是一种数学表达式。")
     sum += 20;
 
     return sum;
